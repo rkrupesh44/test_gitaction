a34c8b3c3f03ec3b43c1caff6e8311a3ff1988b9
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-04-08 16:04:22 +0530

    Create scriptcall.sh

---
+++ .github/workflows/scriptcall.sh
@@ -0,0 +1,5 @@
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+   https://api.github.com/repos/rkrupesh44/test_gitaction/new/main/.github/workflowss/sample.yml/dispatches \
+  -d '{"ref":"feature"}'
