57456069933d60fbac86f535a189b5a920affb06
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-04-08 16:07:37 +0530

    Update scriptcall.sh

--- .github/workflows/scriptcall.sh
+++ .github/workflows/scriptcall.sh
@@ -2,4 +2,4 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
    https://api.github.com/repos/rkrupesh44/test_gitaction/new/main/.github/workflowss/sample.yml/dispatches \
-  -d '{"ref":"feature"}'
+  -d '{"ref":"main"}'
